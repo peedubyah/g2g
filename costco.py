4abab7ff62731daaeb3a3490c280810cc2a57a91
Patrick Sermons <patrick.sermons@gmail.com>
2024-05-18 15:25:59 -0500

    added varshan and costco

--- costco.py
+++ costco.py
@@ -67,15 +67,43 @@ def scrape_duriel_ticket_price():
     logging.info(f"Scraped Duriel Ticket price: {formatted_unit_price}")
     return formatted_unit_price
 
-# Function to send the value to Discord webhook
-def post_to_discord(gold_value, duriel_ticket_price):
+# Function to scrape the Varshan Ticket price
+@retry(**retry_settings)
+def scrape_varshan_ticket_price():
+    url = 'https://sls.g2g.com/offer/search?service_id=0765978e-3fdf-48b4-bed3-184823aa439e&brand_id=lgc_game_26891&filter_attr=33821c26:0a926d8a%7C59dd7f4c:4cce7c55%7C9870fe77:328ce087&page_size=20&group=0&currency=USD&country=US'
+    headers = {
+        'accept': 'application/json, text/plain, /',
+        'accept-language': 'en-US,en;q=0.9',
+        'dnt': '1',
+        'origin': 'https://www.g2g.com/',
+        'referer': 'https://www.g2g.com/',
+        'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0',
+    }
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+    formatted_unit_price = data['payload']['results'][0]['formatted_unit_price']
+    logging.info(f"Scraped Varshan Ticket price: {formatted_unit_price}")
+    return formatted_unit_price
+
+# Function to send the values to Discord webhook
+def post_to_discord(gold_value, duriel_ticket_price, varshan_ticket_price):
     webhook_url = 'https://discord.com/api/webhooks/1213261615005179914/F0HKvm1g2ypxJFrY9MbjRc7is4oVSvOd8lMYMEPhyNpEj3iHuLadJTC1YGwo2YZyZGJt'
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = datetime.now().isoformat()
     embed = {
         "title": "G2G Update",
         "description": (
             f":coin: [**1 Million Gold**](https://www.g2g.com/categories/diablo-4-gold): **${gold_value}**\n"
-            f":ticket: [**Duriel Ticket Price**](https://www.g2g.com/categories/diablo-4-item-buy/offer/group?fa=33821c26%3A0a926d8a%7C59dd7f4c%3Af6477539%7C9870fe77%3A328ce087): **${duriel_ticket_price}**"
+            f":ticket: [**Duriel Ticket Price**](https://www.g2g.com/categories/diablo-4-item-buy/offer/group?fa=33821c26%3A0a926d8a%7C59dd7f4c%3Af6477539%7C9870fe77%3A328ce087): **${duriel_ticket_price}**\n"
+            f":ticket: [**Varshan Ticket Price**](https://www.g2g.com/categories/diablo-4-item-buy/offer/group?fa=33821c26%3A0a926d8a%7C59dd7f4c%3A4cce7c55%7C9870fe77%3A328ce087): **${varshan_ticket_price}**"
         ),
         "color": 15258703,  # Light orange color
         "footer": {
@@ -120,10 +148,11 @@ def main():
     try:
         gold_value = scrape_gold_value()
         duriel_ticket_price = scrape_duriel_ticket_price()
-        if gold_value and duriel_ticket_price:
-            post_to_discord(gold_value, duriel_ticket_price)
+        varshan_ticket_price = scrape_varshan_ticket_price()
+        if gold_value and duriel_ticket_price and varshan_ticket_price:
+            post_to_discord(gold_value, duriel_ticket_price, varshan_ticket_price)
         else:
-            error_message = "Failed to scrape one or both values"
+            error_message = "Failed to scrape one or more values"
             logging.error(error_message)
             post_error_to_discord(error_message)
     except Exception as e:
